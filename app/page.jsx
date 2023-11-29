56cd155cd329b04250b71d1b3bfc37d4afce7979
Nekruz Ashrapov <64292629+NekruzAsh@users.noreply.github.com>
2023-11-28 22:09:04 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -15,11 +15,11 @@ export default function Home() {
       <h2>Company Updates</h2>
 
       <div className="card">
-        <h3>New member of the web dev team...</h3>
+        <h3>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
       </div>
       <div className="card">
-        <h3>New website live!</h3>
+        <h3>wadsdasdwwwwwwwwkkjkjckjsjjwadsd</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
       </div>
     </main>
