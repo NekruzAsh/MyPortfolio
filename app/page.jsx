4749792ecc8b35e03d5ccb20372832794cd937af
NekruzAsh <nekruzashrapov16@gmail.com>
2023-11-28 22:06:31 -0500

    new update

--- app/page.jsx
+++ app/page.jsx
@@ -12,14 +12,14 @@ export default function Home() {
         </Link>
       </div>
         
-      <h2>Company Updates</h2>
+      <h2>About Me</h2>
 
       <div className="card">
-        <h3>New member of the web dev team...</h3>
+        <h3>aaaaaaaaaaaaaaaaaaaaaaa...</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
       </div>
       <div className="card">
-        <h3>New website live!</h3>
+        <h3>aaaaaaaaaaaaaaaaaaa!</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
       </div>
     </main>
