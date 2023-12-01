340ee72bf61c2f77f3b0b379b08b425ea3e98210
NekruzAsh <nekruzashrapov16@gmail.com>
2023-11-30 21:06:46 -0500

    Update

--- app/page.jsx
+++ app/page.jsx
@@ -6,11 +6,8 @@ export default function Home() {
         
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
 
-      <div className="flex justify-center my-8">
-        <Link href="/tickets">
-          <button className="btn-primary">View Tickets</button>
-        </Link>
-      </div>
+
+      
         
       <h2>About Me</h2>
 
