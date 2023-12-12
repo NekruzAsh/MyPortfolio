b1efae1720a7ae130cd77feb06f79b91a72e171f
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-12 18:18:28 -0500

    Update Navbar links

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const navbar = () => {
   return (
     <div className={shadow ? "fixed w-full h-20 shadow-xl z-[100] bg-[#222222]" : "fixed w-full h-20 z-[100] bg-[#222222]"}>
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Link href="/#home" scroll={false}>
+        <Link href="/#home">
         <Image
           src="/images/nwanyzch.png"
           alt="logo"
@@ -39,27 +39,27 @@ const navbar = () => {
         </Link>
         <div>
           <ul className="hidden md:flex">
-            <Link href="/#home" scroll={false}>
+            <Link href="/#home">
               <li className="ml-10 text-sm uppercase hover:border-b hover:border-orange-500">
                 Home
               </li>
             </Link>
-            <Link href="/#about" scroll={false}>
+            <Link href="/#about">
               <li className="ml-10 text-sm uppercase hover:border-b hover:border-orange-500">
                 About
               </li>
             </Link>
-            <Link href="/#skills" scroll={false}>
+            <Link href="/#skills">
               <li className="ml-10 text-sm uppercase hover:border-b hover:border-orange-500">
                 Skills
               </li>
             </Link>
-            <Link href="/#projects" scroll={false}>
+            <Link href="/#projects">
               <li className="ml-10 text-sm uppercase hover:border-b hover:border-orange-500">
                 Projects
               </li>
             </Link>
-            <Link href="/#contact" scroll={false}>
+            <Link href="/#contact">
               <li className="ml-10 text-sm uppercase hover:border-b hover:border-orange-500">
                 Contact
               </li>
@@ -107,19 +107,19 @@ const navbar = () => {
           </div>
           <div className="py-4 flex flex-col">
             <ul className="uppercase">
-              <Link href="/#home" scroll={false}>
+              <Link href="/#home">
                 <li className="py-4 text-sm text-black">Home</li>
               </Link>
-              <Link href="/#about" scroll={false}>
+              <Link href="/#about">
                 <li className="py-4 text-sm text-black">About</li>
               </Link>
-              <Link href="/#skills" scroll={false}>
+              <Link href="/#skills">
                 <li className="py-4 text-sm text-black">Skills</li>
               </Link>
-              <Link href="/#projects" scroll={false}>
+              <Link href="/#projects">
                 <li className="py-4 text-sm text-black">Projects</li>
               </Link>
-              <Link href="/#contact" scroll={false}>
+              <Link href="/#contact">
                 <li className="py-4 text-sm text-black">Contact</li>
               </Link>
             </ul>
