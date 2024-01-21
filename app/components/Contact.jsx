cdb7456b6e3c1110c3f9a64f4fa41cc9d5c82011
NekruzAsh <nekruzashrapov16@gmail.com>
2024-01-21 01:20:27 -0500

    updated social links for different viewports.

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-
-import { IoIosMail } from "react-icons/io";
-import { FaPhoneSquare } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -19,8 +17,10 @@ const Contact = () => {
           Get In Touch
         </h2>
         <p className="text-[#7899BD] text-[17px] py-4 text-center mx-10 mb-10">
-          I am actively seeking a <span className="text-[#f44336]"> Summer 2024 Internship/Co-op</span>. If you have any
-          opportunities, insights, or just want to say hi, please message me. I will get back to you as soon as possible!
+          I am actively seeking a{" "}
+          <span className="text-[#f44336]"> Summer 2024 Internship/Co-op</span>.
+          If you have any opportunities, insights, or just want to say hi,
+          please message me. I will get back to you as soon as possible!
         </p>
         <div className="grid lg:grid-cols-5 gap-8">
           <div className="col-span-full w-full h-auto bg-[#101922] shadow-md shadow-blue-900 rounded-xl lg:p-4">
@@ -54,7 +54,9 @@ const Contact = () => {
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className=" text-md py-2 text-[#7899BD]">Your Subject</label>
+                  <label className=" text-md py-2 text-[#7899BD]">
+                    Your Subject
+                  </label>
                   <input
                     className="text-[#f3f7fc] bg-[#2e3846] rounded-lg p-3 flex"
                     type="text"
@@ -75,23 +77,44 @@ const Contact = () => {
                 </div>
                 <div className="flex justify-center">
                   <button>
-                <a
-                  className="relative flex w-[6rem] h-[50px] text-md mt-2 uppercase px-6 py-3 cursor-pointer text-[#f44336] hover:text-[#011222] group"
-                >
-                  <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-2 translate-y-2 border-2 border-[#f44336] group-hover:bg-[#BFD0E3] group-hover:translate-x-0 group-hover:translate-y-0" />
-                  <span className="absolute inset-0 w-full h-full border-2 border-[#f44336]" />
-                  <span className="relative">send</span>
-                </a>
-                </button>
+                    <a className="relative flex w-[6rem] h-[50px] text-md mt-2 uppercase px-6 py-3 cursor-pointer text-[#f44336] hover:text-[#011222] group">
+                      <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-2 translate-y-2 border-2 border-[#f44336] group-hover:bg-[#BFD0E3] group-hover:translate-x-0 group-hover:translate-y-0" />
+                      <span className="absolute inset-0 w-full h-full border-2 border-[#f44336]" />
+                      <span className="relative">send</span>
+                    </a>
+                  </button>
                 </div>
               </form>
             </div>
           </div>
         </div>
-        <div className="text-sm text-center mt-36 text-[#7899BD] cursor-pointer hover:text-[#f44336] transition-colors duration-500">
-          <a href="https://github.com/NekruzAsh/MyPortfolio.git" target="_blank">Designed and Built by Nekruz Ashrapov</a>
+        <div className="flex justify-center space-x-4 items-center mt-10 lg:hidden md:hidden">
+          <a
+            href="https://github.com/NekruzAsh"
+            target="_blank"
+            className="text-[#7899BD] hover:text-[#f44336] transition-colors duration-500"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/nekruzash/"
+            target="_blank"
+            className="text-[#7899BD] hover:text-[#f44336] transition-colors duration-500"
+          >
+            <FaLinkedinIn size={30} />
+          </a>
         </div>
-          <p className="text-sm text-center text-[#7899BD]">+1 (929) 554-0055</p>
+        <div className="text-sm text-center md:mt-16 mt-4 text-[#7899BD] cursor-pointer hover:text-[#f44336] transition-colors duration-500">
+          <a
+            href="https://github.com/NekruzAsh/MyPortfolio.git"
+            target="_blank"
+          >
+            Designed and Built by Nekruz Ashrapov
+          </a>
+        </div>
+        <p className="text-sm text-center text-[#7899BD] pb-10">
+          +1 (929) 554-0055
+        </p>
       </div>
     </div>
   );
--- app/components/SocialLinks.jsx
+++ app/components/SocialLinks.jsx
@@ -13,11 +13,11 @@ const SocialLinks = () => {
       </a>
       <div className="border-l-2 border-[#7899BD] h-36"></div>
     </div>
-    <div className="fixed right-10 bottom-0 space-y-4 flex flex-col items-center text-center max-md:hidden">
+    <div className="fixed right-1 bottom-0 space-y-4 flex flex-col items-center text-center max-md:hidden">
       <div className='rotate-90 mb-[75px] pb-1 text-sm text-[#7899BD] hover:cursor-pointer hover:text-[#f44336] transition-colors duration-500'>
           <p><a href="mailto:ashrapnz@mail.uc.edu" >ashrapnz@mail.uc.edu</a></p>
       </div>
-      <div className="border-l-2 border-[#7899BD] h-12"></div>
+      <div className="border-l-2 border-[#7899BD] h-16"></div>
     </div>
     </div>
     
