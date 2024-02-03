1ed4be4b7e8ba44fb9da477b7e46596c37e14c07
NekruzAsh <nekruzashrapov16@gmail.com>
2024-02-03 18:32:56 -0500

    added new translate animation for the social links

--- app/components/SocialLinks.jsx
+++ app/components/SocialLinks.jsx
@@ -5,16 +5,16 @@ const SocialLinks = () => {
   return (
     <div>
     <div className="fixed left-10 bottom-0 space-y-4 flex flex-col items-center max-md:hidden">
-      <a href="https://github.com/NekruzAsh" target="_blank" className='text-[#7899BD] hover:text-[#f44336] transition-colors duration-500'>
+      <a href="https://github.com/NekruzAsh" target="_blank" className='text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
         <FaGithub size={24} />
       </a>
-      <a href="https://www.linkedin.com/in/nekruzash/" target="_blank" className='text-[#7899BD] hover:text-[#f44336] transition-colors duration-500'>
+      <a href="https://www.linkedin.com/in/nekruzash/" target="_blank" className='text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
         <FaLinkedinIn size={24} />
       </a>
       <div className="border-l-2 border-[#7899BD] h-36"></div>
     </div>
     <div className="fixed right-[-12px] bottom-0 space-y-4 flex flex-col items-center text-center max-md:hidden">
-      <div className='rotate-90 mb-[75px] pb-1 text-sm text-[#7899BD] hover:cursor-pointer hover:text-[#f44336] transition-colors duration-500'>
+      <div className='rotate-90 mb-[75px] pb-1 text-sm text-[#7899BD] hover:cursor-pointer hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
           <p><a href="mailto:ashrapnz@mail.uc.edu" >ashrapnz@mail.uc.edu</a></p>
       </div>
       <div className="border-l-2 border-[#7899BD] h-16"></div>
