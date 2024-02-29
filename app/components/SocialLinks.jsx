f44ba12c3516cb6fbdcb068fd0d2ae0e9091de36
NekruzAsh <nekruzashrapov16@gmail.com>
2024-02-28 20:11:18 -0500

    added a new devpost link to author's profile for the social links.

--- app/components/SocialLinks.jsx
+++ app/components/SocialLinks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FiGithub, FiLinkedin} from "react-icons/fi";
+import { SiDevpost } from "react-icons/si";
 
 const SocialLinks = () => {
   return (
@@ -11,10 +12,13 @@ const SocialLinks = () => {
       <a href="https://www.linkedin.com/in/nekruzash/" target="_blank" className='text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
       <FiLinkedin size={24} />
       </a>
-      <div className="border-l-2 border-[#7899BD] h-36"></div>
+      <a href="https://devpost.com/NekruzAsh?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" target="_blank" className='text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
+      <SiDevpost size={24} />
+      </a>
+      <div className="border-l-2 border-[#7899BD] h-32"></div>
     </div>
     <div className="fixed right-[-12px] bottom-0 space-y-4 flex flex-col items-center text-center max-md:hidden">
-      <div className='rotate-90 mb-[75px] pb-1 text-sm text-[#7899BD] hover:cursor-pointer hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
+      <div className='rotate-90 mb-[75px] pb-1 text-sm text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
           <p><a href="mailto:ashrapnz@mail.uc.edu" >ashrapnz@mail.uc.edu</a></p>
       </div>
       <div className="border-l-2 border-[#7899BD] h-16"></div>
