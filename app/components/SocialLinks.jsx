f0854dbcf009136eb148b9a34d756c86b2bca82a
NekruzAsh <nekruzashrapov16@gmail.com>
2024-03-01 16:41:38 -0500

    fixed spacing for social link icons.

--- app/components/SocialLinks.jsx
+++ app/components/SocialLinks.jsx
@@ -5,7 +5,7 @@ import { SiDevpost } from "react-icons/si";
 const SocialLinks = () => {
   return (
     <div>
-    <div className="fixed left-10 bottom-0 space-y-4 flex flex-col items-center max-md:hidden">
+    <div className="fixed left-10 bottom-0 space-y-6 flex flex-col items-center max-md:hidden">
       <a href="https://github.com/NekruzAsh" target="_blank" className='text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
       <FiGithub size={24} />
       </a>
@@ -18,10 +18,10 @@ const SocialLinks = () => {
       <div className="border-l-2 border-[#7899BD] h-32"></div>
     </div>
     <div className="fixed right-[-12px] bottom-0 space-y-4 flex flex-col items-center text-center max-md:hidden">
-      <div className='rotate-90 mb-[75px] pb-1 text-sm text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
+      <div className='rotate-90 mb-[75px] pb-1 text-md text-[#7899BD] hover:text-[#f44336] hover:translate-y-[-4px] duration-500'>
           <p><a href="mailto:ashrapnz@mail.uc.edu" >ashrapnz@mail.uc.edu</a></p>
       </div>
-      <div className="border-l-2 border-[#7899BD] h-16"></div>
+      <div className="border-l-2 border-[#7899BD] h-20"></div>
     </div>
     </div>
     
