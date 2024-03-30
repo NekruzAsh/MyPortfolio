e5f9e7fbf7e8260c0b925cb065b34c10c3c3335a
NekruzAsh <nekruzashrapov16@gmail.com>
2024-03-30 15:50:43 -0400

    added new bg 3D design for landing page.

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -7,11 +7,16 @@ const main = () => {
   return (
     <div
       id="home"
-      className="w-full h-screen text-center bg-center bg-cover relative"
-      style={{ backgroundImage: `url(/images/herobg.png)` }}
+      className="w-full h-screen text-center relative"
+      // style={{ backgroundImage: `url(/images/herobg.png)` }}
     >
-      
-      <div className="max-w-[1240px] w-full h-full mx-auto p-4 flex justify-center items-center z-20 relative">
+      <iframe
+        src="https://my.spline.design/theorbhand-8ae8c3c7b68060908fb34260df538c4a/"
+        className="left-0 right-0 bottom-0 w-full h-full absolute z-10"
+        width="100%"
+        height="100%"
+      ></iframe>
+      <div className=" bg-[#050817] bg-opacity-70 w-full h-full mx-auto p-4 flex justify-center items-center z-20 relative">
         <div className="text-left mb-20">
           <p className="text-[#64feda] text-[22px]">Hi, my name is</p>
           <h1 className="py-4 md:text-[60px] text-[#e8ddff] font-bold">
@@ -31,12 +36,12 @@ const main = () => {
             </a>
           </div>
           <div className="flex flex-col transform translate-y-[200%] md:translate-y-[270%]">
-          <div className="scroll-button-wrapper">
-            <a href="/#about">
-              <div className="scroll-button"></div>
-              <div className="scroll-txt"></div>
-            </a>
-          </div>
+            <div className="scroll-button-wrapper">
+              <a href="/#about">
+                <div className="scroll-button"></div>
+                <div className="scroll-txt"></div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
